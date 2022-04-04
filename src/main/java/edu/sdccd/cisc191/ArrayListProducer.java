afed23cdd5efeece48bfd5eeea5cd840950966fa
Benjamin Cayler <benjamincayler@Benjamins-MacBook-Pro.local>
2022-04-04 16:41:32 -0700

    Initial commit

---
+++ src/main/java/edu/sdccd/cisc191/ArrayListProducer.java
@@ -0,0 +1,19 @@
+package edu.sdccd.cisc191;
+
+import java.util.ArrayList;
+
+public class ArrayListProducer
+{
+    private String list;
+
+    public ArrayListProducer(/* TODO fill in the parameter */)
+    {
+        // TODO complete the constructor
+    }
+
+    public void produce(int i, String string)
+    {
+        // TODO implement the method
+    }
+
+}
